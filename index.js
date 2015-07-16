bb919663be1cc65ae6f7433e5734bca57f3956e0
justathoughtor2 <thoughthaven@gmail.com>
2015-07-16 13:01:43 -0400

    Switch to Ghost-App 0.0.2

---
+++ index.js
@@ -0,0 +1,37 @@
+var App = require('ghost-app'), GhostHelpers;
+
+GhostHelpers = App.extend({
+
+  install: function() {},
+
+  uninstall: function() {},
+
+  activate: function() {
+    this.ghost.helpers.register('blog-header-bg', this.handleBlogHeaderBg(image, options));
+    this.ghost.helpers.register('post-loop-bg', this.handlePostLoopBg(image, index, options));
+  },
+
+  deactivate: function() {},
+  
+  handleBlogHeaderBg: function(image, options) {
+    $('.page-header').first().css('background-image', image);
+    $('.page-header-inner').first().css('color', '#433a48');
+    $('.page-header-inner').first().css('background-color', '#645f66');
+    $('.page-header-inner').first().css('opacity', '0.5');
+    $('.page-header-inner').first().css('margin-left', '8%');
+    $('.page-header-inner').first().css('margin-right', '8%');
+    $('.page-header-inner').first().children().css('opacity', '1.0');
+  },
+  
+  handlePostLoopBg: function(image, index, options) {
+    $('.post-loop').eq(index).css('background-image', image);
+    $('.post-loop-inner').eq(index).css('color', '#433a48');
+    $('.post-loop-inner').eq(index).css('background-color', '#645f66');
+    $('.post-loop-inner').eq(index).css('opacity', '0.5');
+    $('.post-loop-inner').eq(index).css('margin-left', '8%');
+    $('.post-loop-inner').eq(index).css('margin-right', '8%');
+    $('.post-loop-inner').eq(index).children().css('opacity', '1.0');
+  }
+});
+
+module.exports = GhostHelpers;
