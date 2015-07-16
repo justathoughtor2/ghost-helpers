016a57b49e84714044eebe96a727f238bc9ae8db
justathoughtor2 <thoughthaven@gmail.com>
2015-07-15 23:32:50 -0400

    Add font color change

--- helpers.js
+++ helpers.js
@@ -1,5 +1,6 @@
 module.exports = function() {
   hbs.registerHelper('blog-header-bg', function(image, options) {
     $('#blog-header').css('background-image', image);
+    $('#blog-header').css('color', '#7f7e7e');
   });
 };
