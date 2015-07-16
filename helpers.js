4f9d4a7c5b8ca094b7f12977956cd1587eec9b13
justathoughtor2 <thoughthaven@gmail.com>
2015-07-16 00:05:18 -0400

    Add support for post-backgrounds

--- helpers.js
+++ helpers.js
@@ -3,4 +3,9 @@ module.exports = function() {
     $('#blog-header').css('background-image', image);
     $('#blog-header').css('color', '#7f7e7e');
   });
+  
+  hbs.registerHelper('post-loop-bg', function(image, index, options) {
+    $('.post-loop').eq(index).css('background-image', image);
+    $('.post-loop').eq(index).css('color', '#7f7e7e');
+  });
 };
